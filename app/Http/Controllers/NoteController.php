3fdaa9e54d3f5540c3d31062433f865afda516e0
MHCelso <furuyamic@gmail.com>
2018-07-16 10:15:36 -0500

    delete note created

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -101,6 +101,8 @@ class NoteController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $note=Note::find($id);
+        $note->delete();
+        return "Eliminado";
     }
 }
